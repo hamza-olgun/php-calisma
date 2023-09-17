2cfdb10daf2dc23f3ca5a2aab0f36feea107cdd6
Hamza Olgun <102774428+hamza-olgun@users.noreply.github.com>
2023-09-17 18:31:29 +0300

    $_FILES super globali kullanımı

---
+++ Kucuk-Projeler/$_FILES super globali kullanımı/cikti.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<meta http-equiv="Content-Type" content="tr">
+<meta charset="utf-8">
+<title>Untitled Dsdfocument</title>
+</head>
+
+<body>
+	<?php
+		$GelenDosya = $_FILES["Dosya"];
+
+		echo "<pre>";
+		print_r($GelenDosya);
+		echo "</pre>";
+	?>
+	
+</body>
+</html>
---
+++ Kucuk-Projeler/$_FILES super globali kullanımı/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<meta http-equiv="Content-Type" content="tr">
+<meta charset="utf-8">
+<title>Untitled Dsdfocument</title>
+</head>
+
+<body>
+	<form action="cikti.php" method="post" enctype="multipart/form-data">
+	Dosya Seçiniz: <input type="file" name="Dosya"><br />
+
+		<input type="submit" value="Dosyayı Yükle">
+	
+	</form>
+	
+</body>
+</html>
