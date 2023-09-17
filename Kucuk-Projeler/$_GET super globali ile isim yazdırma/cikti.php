92d891537d74ec175a356df14820e95f51867ae3
Hamza Olgun <102774428+hamza-olgun@users.noreply.github.com>
2023-09-17 17:41:29 +0300

    $_GET superglobali  ile isim yazdırma

---
+++ Kucuk-Projeler/$_GET super globali ile isim yazdırma/cikti.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<meta http-equiv="Content-Type" content="tr">
+<meta charset="utf-8">
+<title>Untitled Dsdfocument</title>
+</head>
+
+<body>
+	<?php
+		$Gelenisim = $_GET["Ad"];
+		$Gelensoyad = $_GET["Soyad"];
+		
+		echo $Gelenisim . " ". $Gelensoyad;
+	
+
+	?>
+	
+</body>
+</html>
---
+++ Kucuk-Projeler/$_GET super globali ile isim yazdırma/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<meta http-equiv="Content-Type" content="tr">
+<meta charset="utf-8">
+<title>Untitled Dsdfocument</title>
+</head>
+
+<body>
+	<form action="cikti.php" method="get">
+	Ad: <input type="text" name="Ad"><br />
+	Soyad: <input type="text" name="Soyad"><br />
+		<input type="submit" value="Gönder">
+	
+	</form>
+	
+</body>
+</html>
