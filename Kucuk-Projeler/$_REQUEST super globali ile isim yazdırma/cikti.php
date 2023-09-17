f33b862b32643c4beef42675013252c7fa22ba7c
Hamza Olgun <102774428+hamza-olgun@users.noreply.github.com>
2023-09-17 18:09:00 +0300

    $_REQUEST super globali ile isim yazdırma
    $_REQUEST super globali GET, POST isteklerinin ikisinide karşılayabiliyor fakat güvenlik açığına sebebiyet verebiliyor.

---
+++ Kucuk-Projeler/$_REQUEST super globali ile isim yazdırma/cikti.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<meta http-equiv="Content-Type" content="tr">
+<meta charset="utf-8">
+<title>Untitled Dsdfocument</title>
+</head>
+
+<body>
+	<?php
+		$Gelenisim = $_REQUEST["Ad"];
+		$Gelensoyad = $_REQUEST["Soyad"];
+		
+		echo $Gelenisim . " ". $Gelensoyad;
+	
+
+	?>
+	
+</body>
+</html>
